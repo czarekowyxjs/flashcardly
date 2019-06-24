e02d4528218f7eccf237bbae23f63cca8e5b7a1d
czarekowyxjs <cezary.goralski@onet.pl>
2019-06-24 11:09:47 +0200

    update

---
+++ server/src/public/build/precache-manifest.32fe2709be10e049f923fd2723a205b8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b7aebb7e31db6376a6b",
+    "url": "/static/js/4.fc1fe4fe.chunk.js"
+  },
+  {
+    "revision": "4bfd7cbffb64a0d50b3d",
+    "url": "/static/css/main.4a188a93.chunk.css"
+  },
+  {
+    "revision": "697df34e415422527bd1",
+    "url": "/static/js/runtime~main.73770a28.js"
+  },
+  {
+    "revision": "e646dff5184b9872a72b",
+    "url": "/static/js/2.4af97cd5.chunk.js"
+  },
+  {
+    "revision": "e2d3606add6b97e466ebd29bab4af707",
+    "url": "/static/media/Kalam-Bold.e2d3606a.ttf"
+  },
+  {
+    "revision": "ddc878922f38639c67bc",
+    "url": "/static/js/3.f2fb2747.chunk.js"
+  },
+  {
+    "revision": "37d68cdedd7132bd402e12e2e01faca3",
+    "url": "/static/media/Kalam-Light.37d68cde.ttf"
+  },
+  {
+    "revision": "4bfd7cbffb64a0d50b3d",
+    "url": "/static/js/main.6e8cc2ae.chunk.js"
+  },
+  {
+    "revision": "31933dede9b239f63aec",
+    "url": "/static/js/7.b6a49e08.chunk.js"
+  },
+  {
+    "revision": "15edccc3fb6f803c6f0c",
+    "url": "/static/js/5.b0d419ba.chunk.js"
+  },
+  {
+    "revision": "4eaf4563714adb99ecb0",
+    "url": "/static/js/6.e0157e9c.chunk.js"
+  },
+  {
+    "revision": "15edccc3fb6f803c6f0c",
+    "url": "/static/css/5.608fd691.chunk.css"
+  },
+  {
+    "revision": "8b7aebb7e31db6376a6b",
+    "url": "/static/css/4.70e51243.chunk.css"
+  },
+  {
+    "revision": "ddc878922f38639c67bc",
+    "url": "/static/css/3.91d33332.chunk.css"
+  },
+  {
+    "revision": "507025baac2c809040b6d93e7ed3ff7a",
+    "url": "/index.html"
+  }
+];
